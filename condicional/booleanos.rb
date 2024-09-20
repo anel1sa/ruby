0f1e5379fbcf41e057cdb72105f69ca17dd5c83d
Anelisa Lobo <anelisa.lobo@copel.com>
2024-09-20 13:44:42 -0300

    mudança da pasta condicionais para condicional

---
+++ condicional/booleanos.rb
@@ -0,0 +1,21 @@
+'''
+AND 
+V e V = V
+V e F = F
+F e F = F
+F e V = F
+
+OR 
+V ou V = V
+V ou F = V
+F ou F = F
+F ou V = V
+
+NOT
+Not V = F
+Not F = V
+'''
+
+puts false && true
+puts false || true
+puts !false && true
---
+++ condicional/if.rb
@@ -0,0 +1,9 @@
+x = 50
+
+if x == 10
+  puts "x é igual a 10"
+elsif x == 50
+  puts "x é igual a 50"
+else
+  puts "x não é igual a 10"
+end
